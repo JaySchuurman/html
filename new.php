f5d21fa352a7c3cda33eefa1e775a7f620680792
Jay Schuurman <1208455@student.roc-nijmegen.nl>
2024-05-29 11:44:01 +0200

    test
    test

--- new.php
+++ new.php
@@ -1,1 +1,26 @@
-wasaaaaaaaah
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sky Flights</title>
+    <link rel="stylesheet" href="css/new.css">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <div class="logo">SKY FLIGHTS</div>
+        
+    </div>
+<div class="row">
+    <div class="home-button">
+            <div class="home-button-text">Home</div>
+    <div class="logout-button">
+            <div class="logout-button-text">Logout</div>
+    </div>
+</div>
+</body>
+</html>
